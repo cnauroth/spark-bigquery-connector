b94bbf471bd0af6ebd7a02b7cca83c7f6cf51b41
Chris Nauroth <cnauroth@google.com>
2024-10-24 22:12:15 +0000

    Use DirectBigQueryRelation, similar to the DataSource v1 path.

--- spark-bigquery-connector-common/src/main/java/com/google/cloud/spark/bigquery/write/CreatableRelationProviderHelper.java
+++ spark-bigquery-connector-common/src/main/java/com/google/cloud/spark/bigquery/write/CreatableRelationProviderHelper.java
@@ -15,16 +15,26 @@
  */
 package com.google.cloud.spark.bigquery.write;
 
+import static com.google.cloud.bigquery.TableDefinition.Type.EXTERNAL;
+import static com.google.cloud.bigquery.TableDefinition.Type.MATERIALIZED_VIEW;
+import static com.google.cloud.bigquery.TableDefinition.Type.SNAPSHOT;
+import static com.google.cloud.bigquery.TableDefinition.Type.TABLE;
+import static com.google.cloud.bigquery.TableDefinition.Type.VIEW;
 import static com.google.cloud.spark.bigquery.SparkBigQueryUtil.scalaMapToJavaMap;
 
 import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.TableDefinition.Type;
+import com.google.cloud.bigquery.TableInfo;
 import com.google.cloud.bigquery.connector.common.BigQueryClient;
+import com.google.cloud.bigquery.connector.common.BigQueryClientFactory;
 import com.google.cloud.bigquery.connector.common.BigQueryUtil;
+import com.google.cloud.bigquery.connector.common.LoggingBigQueryTracerFactory;
 import com.google.cloud.spark.bigquery.DataSourceVersion;
 import com.google.cloud.spark.bigquery.InjectorBuilder;
 import com.google.cloud.spark.bigquery.SchemaConverters;
 import com.google.cloud.spark.bigquery.SchemaConvertersConfiguration;
 import com.google.cloud.spark.bigquery.SparkBigQueryConfig;
+import com.google.cloud.spark.bigquery.direct.DirectBigQueryRelation;
 import com.google.cloud.spark.bigquery.write.context.BigQueryDataSourceWriterModule;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Injector;
@@ -86,8 +96,8 @@ public class CreatableRelationProviderHelper {
       SaveMode saveMode,
       Map<String, String> customDefaults) {
     Map<String, String> properties = scalaMapToJavaMap(parameters);
-    BigQueryInsertableRelationBase relation =
-        createBigQueryInsertableRelation(sqlContext, properties, schema, saveMode, customDefaults);
+    BaseRelation relation =
+        createDirectBigQueryRelation(sqlContext, properties, schema, saveMode, customDefaults);
     return relation;
   }
 
@@ -112,7 +122,7 @@ public class CreatableRelationProviderHelper {
   }
 
   @VisibleForTesting
-  BigQueryInsertableRelationBase createBigQueryInsertableRelation(
+  DirectBigQueryRelation createDirectBigQueryRelation(
       SQLContext sqlContext,
       Map<String, String> properties,
       StructType schema,
@@ -128,7 +138,49 @@ public class CreatableRelationProviderHelper {
             .withTableIsMandatory(true)
             .build();
 
-    return createBigQueryInsertableRelationInternal(sqlContext, schema, saveMode, injector);
+    SparkBigQueryConfig config = injector.getInstance(SparkBigQueryConfig.class);
+    BigQueryClient bigQueryClient = injector.getInstance(BigQueryClient.class);
+    Schema bigQuerySchema =
+        SchemaConverters.from(SchemaConvertersConfiguration.from(config)).toBigQuerySchema(schema);
+    bigQueryClient.createTableIfNeeded(config.getTableId(), bigQuerySchema, config);
+    TableInfo tableInfo = bigQueryClient.getReadTable(config.toReadTableOptions());
+    String tableName = BigQueryUtil.friendlyTableName(config.getTableId());
+    BigQueryClientFactory bigQueryReadClientFactory =
+        injector.getInstance(BigQueryClientFactory.class);
+    LoggingBigQueryTracerFactory bigQueryTracerFactory =
+        injector.getInstance(LoggingBigQueryTracerFactory.class);
+    // if (tableInfo == null) {
+    //   throw new RuntimeException("Table " + tableName + " not found");
+    // }
+    Type tableType = tableInfo.getDefinition().getType();
+    if (tableType == TABLE || tableType == EXTERNAL || tableType == SNAPSHOT) {
+      return new DirectBigQueryRelation(
+          config,
+          tableInfo,
+          bigQueryClient,
+          bigQueryReadClientFactory,
+          bigQueryTracerFactory,
+          sqlContext);
+    } else if (tableType == VIEW || tableType == MATERIALIZED_VIEW) {
+      if (!config.isViewsEnabled()) {
+        throw new RuntimeException(
+            "Views were not enabled. You can enable views by setting '"
+                + SparkBigQueryConfig.VIEWS_ENABLED_OPTION
+                + "' to true. Notice additional cost may occur.");
+      }
+      return new DirectBigQueryRelation(
+          config,
+          tableInfo,
+          bigQueryClient,
+          bigQueryReadClientFactory,
+          bigQueryTracerFactory,
+          sqlContext);
+    } else {
+      throw new UnsupportedOperationException(
+          String.format(
+              "The type of table %s is currently not supported: %s",
+              tableName, tableInfo.getDefinition().getType()));
+    }
   }
 
   public BigQueryInsertableRelationBase createBigQueryInsertableRelation(
